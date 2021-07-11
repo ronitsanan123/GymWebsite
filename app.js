ee856a76906ba6a8478ddc83a9e33846017a9a27
ronitsanan123 <46322871+ronitsanan123@users.noreply.github.com>
2021-07-11 16:47:12 +0530

    My Gym Website

---
+++ app.js
@@ -0,0 +1,70 @@
+// IMPORTING MODULES
+const express = require('express');  
+const path = require('path');  
+const fs= require('fs');  
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost/contactGym', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const app = express();
+const port = 80;
+const hostname = '127.0.0.1';
+
+app.use('/static', express.static('static')); // images a static file this is why we included this 
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug'); // Set the template engine as pug 
+app.set('views', path.join(__dirname, 'views')); // Setting the directory where pug is 
+
+//CREATING CONNECTION WITH MONGOOSE
+const db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("sucessfully connected to mongooose");
+});
+
+// Defining the Schema
+const gymSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    phone: String,
+    address: String,
+    more: String
+  });
+
+// Making the model
+const gym = mongoose.model('contact', gymSchema);
+
+
+// ALL GET REQUESTS HERE
+app.get('/', (req, res) => {
+    res.status(200);
+    console.log("this is get request");
+    res.render('home.pug');
+    // res.send("this is the home page ")
+});
+
+app.get('/views/contact.pug', (req, res) => {
+    res.status(200);
+    console.log("this is get request");
+    res.render('contact.pug');
+    // res.send("this is the home page ")
+});
+
+// ALL POST REQUESTS HERE 
+app.post('/contact', (req, res) => {
+
+    
+    var gymData = new gym (req.body);
+    gymData.save().then(()=>{
+        console.log("Data saved");  
+        res.send("daTA is saved")
+    }).catch(()=>{
+        res.status(404);
+        console.log("Data not saved");
+    });
+});
+
+app.listen(port, () => {
+    console.log(`The server is running at http://${hostname}:${port}/`);
+});
